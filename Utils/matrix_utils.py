48874b4cf89362fb97930175a8dbf9a96be8d92a
Alec <alec.kb@outlook.com>
2025-10-07 17:11:54 +0100

    utils

--- Utils/matrix_utils.py
+++ Utils/matrix_utils.py
@@ -4,3 +4,8 @@ def step(pos, direction):
 
 def within_bounds(grid, pos):
     return pos[0] >=0 and pos[0] < len(grid) and pos[1] >= 0 and pos[1] < len(grid[0])
+
+def get_valid_neighbours(grid, i, j):
+    neighbours = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
+    neighbours = [x for x in neighbours if within_bounds(grid, x)]
+    return neighbours
