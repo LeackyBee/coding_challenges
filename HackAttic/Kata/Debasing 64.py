7174261bd25d7c271e8cab7588d99bb5952de5a4
alec.kb <alec.kb@outlook.com>
2025-10-19 15:53:37 +0100

    Debasing 64

---
+++ HackAttic/Kata/Debasing 64.py
@@ -0,0 +1,23 @@
+import re
+
+
+B64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+B64_INDEX = {ch: i for i, ch in enumerate(B64_ALPHABET)}
+
+def main():
+    lines = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        lines.append(line)
+    for string in lines:
+        bits = [format(B64_ALPHABET.index(char), "06b") for char in list(string) if char != "="]
+        bits_str = "".join(bits)
+        eight_bits = re.findall("........", bits_str)
+        chars = [chr(int(x,2)) for x in eight_bits]
+        print("".join(chars))
+
+if __name__ == "__main__":
+    main()
