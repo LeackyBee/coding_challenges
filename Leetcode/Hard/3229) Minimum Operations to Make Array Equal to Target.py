648ae192ed284fe662984e68949063d4cde33b03
alec.kb <alec.kb@outlook.com>
2025-10-30 19:47:28 +0000

    leetcode 3229

---
+++ Leetcode/Hard/3229) Minimum Operations to Make Array Equal to Target.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int], target: List[int]) -> int:
+        for i in range(len(nums)):
+            target[i] -= nums[i]
+        counter = 0
+        prev = 0
+        for i in range(len(target)):
+            val = target[i]
+            if val > prev:
+                counter += val - prev
+
+            prev = val
+
+        if prev < 0:
+            counter -= prev
+
+        return counter
+
+
+if __name__ == "__main__":
+    assert Solution().minimumOperations([3,5,1,2],[4,6,2,4]) == 2
+    assert Solution().minimumOperations([1,3,2], [2,1,4]) == 5
+    assert Solution().minimumOperations([1,1,3,4], [4,1,3,2]) == 5
