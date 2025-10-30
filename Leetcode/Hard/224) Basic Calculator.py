32424e72c6835697ec016155aee1154cfe81a927
alec.kb <alec.kb@outlook.com>
2025-10-30 23:12:02 +0000

    Delete other Basic Calculator Attempts

--- Leetcode/Hard/224) Basic Calculator.py
+++ Leetcode/Hard/224) Basic Calculator.py
@@ -1,118 +1,40 @@
-class Expression:
-    def evaluate(self) -> int:
-        pass
-
-
-class IntegerExpr(Expression):
-    def __init__(self, value):
-        self.value = value
-
-    def evaluate(self) -> int:
-        return self.value
-
-    def __str__(self):
-        return str(self.value)
-
-
-class AdditionExpr(Expression):
-    def __init__(self, a: Expression, b: Expression):
-        self.a = a
-        self.b = b
-
-    def evaluate(self) -> int:
-        return self.a.evaluate() + self.b.evaluate()
-
-    def __str__(self):
-        return f"({self.a}+{self.b})"
-
-
-class SubtractionExpr(Expression):
-    def __init__(self, a: Expression, b: Expression):
-        self.a = a
-        self.b = b
-
-    def evaluate(self) -> int:
-        return self.a.evaluate() - self.b.evaluate()
-
-    def __str__(self):
-        return f"({self.a}-{self.b})"
-
-
 class Solution:
-    def parse_int(self, chars: list, i: int) -> None:
-        # given the starting index of an integer
-        # pop all the digits off and replace the index with the full integer
-        val = 0
-        while chars and chars[i].isnumeric():
-            val = val * 10 + int(chars.pop(i))
-        chars.insert(i, IntegerExpr(val))
-
-    def parse_subexpression(self, chars: list, index: int):
-        chars.pop(index)  # will be a "("
-        # There will be no two consecutive operators in the input.
-        # because of this, only the first character in s can be the unary negation
-        if chars[index] == "-":
-            # first char is unary negation
-            # -x == 0-x
-            # therefore add a 0 at pos i
-            chars.insert(index, "0")
-
-
-        i = index
-        while chars[i] != ")":
-            if type(chars[i]) is str and chars[i].isnumeric():
-                # [..., "1","2","3",...] -> [..., 123, ...]
-                self.parse_int(chars, i)
-            elif chars[i] == "(":
-                self.parse_subexpression(chars, i)
-            i += 1
-
-        end = i
-
-        i = index
-        while i < end:
-            char = chars[i]
-            if char == "+":
-                b = chars.pop(i+1)
-                chars.pop(i)
-                a = chars.pop(i-1)
-
-                chars.insert(i-1, AdditionExpr(a,b))
-                i -= 1
-                end -= 2
-            elif char == "-":
-                b = chars.pop(i + 1)
-                chars.pop(i)
-                a = chars.pop(i - 1)
-
-                chars.insert(i-1, SubtractionExpr(a, b))
-                i -= 1
-                end -= 2
-            else:
-                i += 1
-        # remove bracket
-        chars.pop(i)
-
     def calculate(self, s: str) -> int:
-        s = s.replace(" ", "")  # remove all spaces
-        chars = ["("] + list(s) + [")"]  # add terminator
+        # carries the sign applied to the result of the parenthesis
+        parenthesis_stack = []
+        # carries the current value before we entered the parenthesis
+        curr_stack = []
 
-        # assert that we only see ) if we've seen a ( beforehand
-        # So if we see a ), we should return the list and the current index
-        # can then parse the string in-place, recursing into the substring when we encounter a (
-        # if we see an operator, the left
-        self.parse_subexpression(chars, 0)
+        s = s.replace(" ", "") + "$" # terminator
+        sign = 1
+        running = 0
+        curr = 0
+        for char in s:
+            if char.isnumeric():
+                running = running*10 + int(char)
+            else:
+                curr += sign * running
+                running = 0
+                if char == "(":
+                    parenthesis_stack.append(sign)
+                    curr_stack.append(curr)
+                    sign = 1
+                    curr = 0
+                elif char == ")":
+                    val = parenthesis_stack.pop() * curr
+                    curr = curr_stack.pop() + val
+                else:
+                    sign = 1 if char == "+" else -1
 
-        return chars[0].evaluate()
-
+        return curr
 
 if __name__ == "__main__":
     solution = Solution()
 
+    assert solution.calculate("0-2147483647") != 0
     assert solution.calculate("10+1+(100-53)") == 58
     assert solution.calculate("1 + 1") == 2
     assert solution.calculate(" 2-1 + 2 ") == 3
     assert solution.calculate("(1+(4+5+2)-3)+(6+8)") == 23
     assert solution.calculate("-10") == -10
-
-
+    assert solution.calculate("1-(     -2)") == 3
