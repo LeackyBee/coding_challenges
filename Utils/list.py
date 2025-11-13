dbd0febf91477bf92ded404132875f220f4a5654
alec.kb <alec.kb@outlook.com>
2025-11-13 12:31:05 +0000

    Leetcode 146

--- Utils/list.py
+++ Utils/list.py
@@ -22,3 +22,21 @@ class ListNode:
 
     def __eq__(self, other):
         return self.val == other.val and self.next == other.next
+
+class DoubleListNode:
+    def __init__(self, value, left:'DoubleListNode'=None, right:'DoubleListNode'=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def recursive_str(self):
+        return str(self) + " ||| " + self.right.recursive_str()
+
+    def __str__(self) -> str:
+        out = ""
+        if self.left:
+            out += f"{self.left.value} <- "
+        out += str(self.value)
+        if self.right:
+            out += f" -> {self.right.value}"
+        return out
