58f763ee6a18e7665407e1451b171c8845257a2f
alec.kb <alec.kb@outlook.com>
2025-11-24 09:20:10 +0000

    leetcode 1018

---
+++ Leetcode/Easy/1018) Binary Prefix Divisible By 5.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        output = []
+        curr = 0
+        for i in range(len(nums)):
+            curr = (curr * 2 + nums[i]) % 5
+            output.append(curr == 0)
+
+        return output
