78f432f7db597613ae2da882b788ff2bc02eb048
alec.kb <alec.kb@outlook.com>
2025-12-03 09:51:16 +0000

    AOC 2025 3

---
+++ AdventOfCode/2025/3) Lobby.py
@@ -0,0 +1,47 @@
+import io
+from curses.ascii import isdigit
+from typing import List
+
+from Utils.parse_utils import parse_file_to_lines
+from Utils.logger import logger
+
+
+
+def findMaxJoltage(banks:List[str], batteries:int):
+    output = 0
+    for bank in banks:
+        prevI = -1
+        joltage = ""
+        for i in range(batteries):
+            maxBattery = "0"
+            for i in range(prevI+1, len(bank) - batteries + i + 1):
+                if bank[i] > maxBattery:
+                    maxBattery = bank[i]
+                    prevI = i
+                    if maxBattery == "9":
+                        break
+            joltage += maxBattery
+        logger.debug(f"Max for {"".join(bank)} is {joltage}")
+        output += int(joltage)
+
+    return output
+
+
+
+if __name__ == "__main__":
+    filepath = input("Input File Path: ")
+
+    file = io.StringIO("""987654321111111
+811111111111119
+234234234234278
+818181911112111""")
+
+    try:
+        file = open(filepath)
+    except:
+        pass
+
+    logger.enable()
+    lines = parse_file_to_lines(file)
+
+    logger.print(findMaxJoltage(lines, 2))
